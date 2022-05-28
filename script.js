9a4604beb47906720dc4dc2e1989559682c0041a
kenizaya <harveylstark@gmail.com>
2022-05-28 06:05:44 +0530

    Increase board and font size, align the board to center, set data-attr to get index of rows and column

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 const Gameboard = (() => {
     const gameboard = [['X', 'O', 'X'],
-                        ['O', 'X', 'O'],
+                        ['O', ' ', 'O'],
                         ['O', 'X', 'O']];
 
     const render = () => {
         const board = document.querySelector(".board");
-        let cc = "";
+        let rowIndex = 0;
+        let columnIndex = 0;
 
         gameboard.forEach(row => {
 
             const div = document.createElement("div");
             let span;
             div.classList.add("row");
+            div.dataset.attr = rowIndex;
             board.appendChild(div);
+            rowIndex++;
+            columnIndex = 0;
 
             row.forEach(column => {
                 span = document.createElement("span");
                 span.textContent = column;
                 console.log(column);
                 span.classList.add("column");
+                span.dataset.attr = columnIndex;
                 div.appendChild(span);
+                columnIndex++;
             });
         });
     }
 
     return {render};
-
-
 })();
 
 
 const Player = (name) => {
     return {name};
 }
+
+const Game = (() => {
+
+})
